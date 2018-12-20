f4415ee9404dd7bc276ff588f898c82fe481d5dd
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-20 04:05:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -21,7 +21,8 @@ def long_planeteer_calls(collection)
   end
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredients.find do |item
 end
