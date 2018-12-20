3f63cbe11804cb3efca6c01c329188cf1415aee6
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-20 04:07:42 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,5 +24,6 @@ end
 def find_the_cheese(ingredients)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
-  ingredients.find do |item
+  ingredients.find do |item|
+    
 end
