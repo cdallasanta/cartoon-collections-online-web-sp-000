90c1d38adafce9ad5c08712c9d7431a6eaa5e173
Chris Dalla Santa <chris.dallasanta@gmail.com>
2018-12-20 03:51:33 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,10 @@ def roll_call_dwarves(dwarves)# code an argument here
 end
 
 def summon_captain_planet(collection)
-  answer = collection.map! do |item|
+  collection.map! do |item|
     item << "!"
     item.capitalize
   end
-  answer
 end
 
 def long_planeteer_calls(collection)
